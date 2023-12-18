13ec73f77d6ceec9d186a5d20bde28545f93ecf7
yura4ka <yura4ka05@gmail.com>
2023-12-18 16:23:44 +0200

    feat: avatar component loads fallback image on error

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from "react";
 import cl from "./Avatar.module.scss";
+import fallback from "@/assets/images/photo-cover.svg";
 
 type Props = {
   src: string;
+  size?: number;
 };
 
-const Avatar = ({ src }: Props) => {
-  return <img className={cl.avatar} src={src} loading="lazy" />;
+const Avatar = ({ src, size = 70 }: Props) => {
+  const [url, setUrl] = useState(src);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    setUrl(src);
+  }, [src]);
+
+  const handleError = () => {
+    if (isError) return;
+    setUrl(fallback);
+    setIsError(true);
+  };
+
+  return (
+    <img
+      width={size}
+      className={cl.avatar}
+      src={url}
+      loading="lazy"
+      onError={handleError}
+    />
+  );
 };
 export default Avatar;
